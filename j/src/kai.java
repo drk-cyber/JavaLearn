2374e789ec3e307860f0421dcd32600325a079c3
drk-cyber <1259192288@qq.com>
2026-03-16 15:54:26 +0800

    11

--- j/src/kai.java
+++ j/src/kai.java
@@ -2,5 +2,7 @@ public class kai {
     public static void main(String[] args) {
         String kk = "a";
         System.out.printf(kk);
+        int a=10;
+        System.out.printf("%d",a);
     }
 }
